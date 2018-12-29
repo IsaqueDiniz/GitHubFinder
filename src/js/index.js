98bea6529b8bd02a745cb7eaa49396de4601761f
IsaqueDiniz <isaquediniz3713@gmail.com>
2018-12-29 13:34:09 -0200

    Ajuste nas chamadas fetch

--- src/js/index.js
+++ src/js/index.js
@@ -3,58 +3,40 @@
 	const URL = 'https://api.github.com/users';
 	const client_id = 'fc9240b12ba087ad4f87';
 	const client_secret = '99eceeffc6cdf1c3740e43adec27de40703447bf';
+	const authKey = `client_id=${client_id}&client_secret=${client_secret}`
 
 	async function getUser(user, callback) {
-		const current =	await fetch(
-			`${URL}/${user}?client_id=${client_id}&client_secret=${client_secret}`
-			).then(res => res.json());
+		const profileResponse =  await fetch(
+			`${URL}/${user}?${authKey}`
+		);
+		const reposResponse = await fetch(
+			`${URL}/${user}/repos?per_page=5&sort=created: desc&${authKey}`
+		);
 
-		callback(current);
+		const profile = profileResponse.json()
+		const repos = reposResponse.json();
+
+		return { profile, repos };
 	}
 
-	function constructView(data) {
-		const $box = document.getElementById('resultContainer');
-		const { avatar_url, public_repos, followers, html_url, location } = data;
-		const pais = location || 'Desconhecida';
-
-		const html = `	
-			 <div class="user profile">
-          <figure class="user ">
-            <img src="${avatar_url}"  title="user-pick">
-          </figure>
-        <div class="user">
-          <ul>
-            <li>Repositórios: 
-	            <span id="repos">${public_repos}</span>
-            </li>
-            <li>Seguidores: 
-            	<span id="seguidores">${followers}</span>
-            	</li>
-            <li>Localização:
-            	<span id="repos">'${pais}'</span>
-            </li>
-          </ul>
-            <a class="user" href="${html_url}" target="_blank">Perfil</a>
-        </div>
-      </div>
-      <div class="user repos">        
-      </div>		
-		`	;		
-
-		$box.innerHTML = html;
+	function userProfileView(profile) {
+		profile.then(res => console.log(res))
 	}
 
-	function clearAll(){
-		document.getElementById('resultContainer').innerHTML = ''
+	function userReposView(repo) {
+		repo.then(res => console.log(res));
 	}
 
 	$search.addEventListener('keyup', evt => {
 		const value = evt.target.value;
 		
 		if(value.length > 3){
-			getUser(value, constructView);
+			getUser(value).then(res =>{
+				userProfileView(res.profile);
+				userReposView(res.repos);
+			});
 		}else {
-			clearAll();
+			document.getElementById('resultContainer').innerHTML = ''
 		}
 	})
 })();
